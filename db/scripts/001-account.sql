39b2319be18bd5dfa1d93258130b3f0a126188d1
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-12 22:18:56 -0500

    feat: add some basic account tables

---
+++ db/scripts/001-account.sql
@@ -0,0 +1,20 @@
+CREATE TABLE account (
+    account_id UUID PRIMARY KEY DEFAULT uuidv4(),
+    name VARCHAR(64) UNIQUE NOT NULL
+);
+
+CREATE TABLE account_login (
+    account_id UUID PRIMARY KEY REFERENCES account (account_id) ON DELETE CASCADE,
+    password TEXT
+);
+
+CREATE FUNCTION fn_account_login_insert() RETURNS trigger AS $$
+BEGIN
+    INSERT INTO account_login (account_id) VALUES (new.account_id);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tg_account_login_insert
+    AFTER INSERT ON account
+    FOR EACH ROW
+    EXECUTE FUNCTION fn_account_login_insert();

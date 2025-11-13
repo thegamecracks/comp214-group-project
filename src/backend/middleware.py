218e542a5cc50d41046b822949ac08efd9af7dbd
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-13 09:22:01 -0500

    feat: add ratelimit middleware

--- src/backend/middleware.py
+++ src/backend/middleware.py
@@ -1,6 +1,10 @@
 import os
 
 from fastapi import FastAPI
+from ratelimit import RateLimitMiddleware, Rule
+from ratelimit.auths.ip import EmptyInformation, client_ip
+from ratelimit.backends.simple import MemoryBackend
+from ratelimit.types import Scope
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 
@@ -14,3 +18,18 @@ def add_middleware(app: FastAPI) -> None:
 
     app.add_middleware(CORSMiddleware)
     app.add_middleware(TrustedHostMiddleware, allowed_hosts=allowed_hosts)
+    app.add_middleware(
+        RateLimitMiddleware,
+        authenticate=_ratelimit_authenticate,
+        backend=MemoryBackend(),
+        config={
+            r"^/": [Rule(minute=60)],
+        },
+    )
+
+
+async def _ratelimit_authenticate(scope: Scope) -> tuple[str, str]:
+    try:
+        return await client_ip(scope)
+    except EmptyInformation:
+        return "global", "default"

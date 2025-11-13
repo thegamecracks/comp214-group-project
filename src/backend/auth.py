f138a7311efa08510ca60921f0d9841b6bd7faac
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-12 23:10:29 -0500

    feat: create admin user on backend startup

---
+++ src/backend/auth.py
@@ -0,0 +1,3 @@
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
--- src/backend/lifespan.py
+++ src/backend/lifespan.py
@@ -1,6 +1,7 @@
 import os
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
+import uuid
 
 import asyncpg
 from fastapi import FastAPI
@@ -22,6 +23,7 @@ async def lifespan(app: FastAPI):
     global _pool
 
     async with _connect_pool() as _pool:
+        await _ensure_admin_user()
         yield
 
 
@@ -30,3 +32,25 @@ async def _connect_pool() -> AsyncIterator[asyncpg.Pool]:
     dsn = os.environ.pop("BACKEND_DB_URI")
     async with asyncpg.create_pool(dsn, min_size=1, max_size=3) as pool:
         yield pool
+
+
+async def _ensure_admin_user() -> None:
+    from .auth import password_hash
+
+    user = os.environ.pop("BACKEND_ADMIN_USER")
+    password = os.environ.pop("BACKEND_ADMIN_PASS")
+
+    async with acquire() as conn:
+        account_id = await conn.fetchval(
+            "INSERT INTO account (name) VALUES ($1) ON CONFLICT DO NOTHING "
+            "RETURNING account_id",
+            user,
+        )
+        if account_id is None:
+            return  # Account already exists
+
+        await conn.execute(
+            "UPDATE account_login SET password = $1 WHERE account_id = $2",
+            password_hash.hash(password),
+            account_id,
+        )

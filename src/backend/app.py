8dec1188edecad76928eab51225a7f6d8b2dd071
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-12 23:52:15 -0500

    feat: add /auth/token endpoint

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from .lifespan import lifespan
+from .routes import auth
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(auth.router)
 
 
 @app.get("/")
---
+++ src/backend/routes/auth.py
@@ -0,0 +1,67 @@
+import datetime
+import os
+from typing import Annotated
+
+import asyncpg
+import jwt
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+from backend.auth import password_hash
+from backend.lifespan import acquire_transaction
+
+JWT_SECRET = os.environ.pop("BACKEND_JWT_SECRET")
+
+router = APIRouter(prefix="/auth")
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+
+
+@router.post("/token")
+async def token(
+    conn: Annotated[asyncpg.Connection, Depends(acquire_transaction)],
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> TokenResponse:
+    """Request a JSON Web Token for the given username and password."""
+    unauthorized_error = HTTPException(
+        401,
+        "Incorrect username or password",
+        {"WWW-Authenticate": "Bearer"},
+    )
+
+    row = await conn.fetchrow(
+        "SELECT account_id, password FROM account "
+        "JOIN account_login USING (account_id) "
+        "WHERE name = $1",
+        form_data.username,
+    )
+    if row is None:
+        raise unauthorized_error
+
+    valid, updated_hash = password_hash.verify_and_update(
+        form_data.password, row["password"]
+    )
+    if not valid:
+        raise unauthorized_error
+
+    if updated_hash:
+        await conn.execute(
+            "UPDATE account_login SET password = $1 WHERE account_id = $2",
+            updated_hash,
+            row["account_id"],
+        )
+
+    payload = {
+        "sub": row["account_id"],
+        "exp": utcnow() + datetime.timedelta(minutes=15),
+    }
+    token = jwt.encode(payload, JWT_SECRET)
+    return TokenResponse(access_token=token, token_type="Bearer")
+
+
+def utcnow() -> datetime.datetime:
+    return datetime.datetime.now(datetime.timezone.utc)

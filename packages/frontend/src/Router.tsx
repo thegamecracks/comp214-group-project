2c61bbc10a6f55af93280828b49245f50daeacd1
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-15 23:37:21 -0500

    style(frontend): fix inconsistent indentation

--- packages/frontend/src/Router.tsx
+++ packages/frontend/src/Router.tsx
@@ -4,15 +4,15 @@ import Frame from "./Frame"
 import Home from "./Home"
 
 function Router() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Frame />}>
-            <Route index element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Frame />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default Router

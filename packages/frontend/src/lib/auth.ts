68c8eb4de590a896a6e601a827754e0cfd642ff4
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-24 18:47:00 -0500

    feat(frontend): add helper auth fetch methods

--- packages/frontend/src/lib/auth.ts
+++ packages/frontend/src/lib/auth.ts
@@ -29,13 +29,34 @@ export class Auth {
     return `${BASE_URL}${path}`
   }
 
-  async authFetch(input: RequestInfo | URL, init?: RequestInit | undefined) {
+  async fetch(path: string, init?: RequestInit | undefined) {
     if (!this.isLoggedIn()) throw new Error("Missing authorization token")
 
     init = init || {}
+    const input = this.getFullURL(path)
     const headers = new Headers(init.headers || {})
     headers.set("Authorization", this.token)
     return await fetch(input, { ...init, headers })
+  }
+
+  async get(path: string, init?: RequestInit | undefined) {
+    return await this.fetch(path, { ...init, method: "GET" })
+  }
+
+  async post(path: string, init?: RequestInit | undefined) {
+    return await this.fetch(path, { ...init, method: "POST" })
+  }
+
+  async put(path: string, init?: RequestInit | undefined) {
+    return await this.fetch(path, { ...init, method: "PUT" })
+  }
+
+  async patch(path: string, init?: RequestInit | undefined) {
+    return await this.fetch(path, { ...init, method: "PATCH" })
+  }
+
+  async delete(path: string, init?: RequestInit | undefined) {
+    return await this.fetch(path, { ...init, method: "DELETE" })
   }
 }
 export const AuthContext = createContext<Auth | null>(null)

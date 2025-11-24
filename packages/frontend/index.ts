b23785f62de139ef31eecc165b2e9bf043fa7457
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-24 18:13:12 -0500

    feat(frontend): print frontend address on startup

--- packages/frontend/index.ts
+++ packages/frontend/index.ts
@@ -1,7 +1,12 @@
 import index from "./src/index.html"
 
+const hostname = process.env.BUN_HOST || "localhost"
+const port = process.env.BUN_PORT || "3000"
+
+console.log(`Starting frontend on: http://${hostname}:${port}`)
 Bun.serve({
-    hostname: process.env.BUN_HOST || "localhost",
+    hostname,
+    port,
     routes: {
         "/": index,
     },

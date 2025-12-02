b068e3a4af07fd599b1077c43a61f726df8183be
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-12-02 12:13:10 -0500

    feat(backend): persist authentication in /docs

--- packages/backend/src/backend/app.py
+++ packages/backend/src/backend/app.py
@@ -5,7 +5,10 @@ from .lifespan import lifespan
 from .middleware import add_middleware
 from .routes import include_routers
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    swagger_ui_parameters={"persistAuthorization": True},
+)
 include_routers(app)
 add_middleware(app)
 

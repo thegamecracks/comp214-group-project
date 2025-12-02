1f00d9f25e8ff80e5078951875d907d3608f91e9
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-12-02 13:36:32 -0500

    feat(db)!: add new_job(max_salary) parameter

--- db/scripts/005-job-procedure.sql
+++ db/scripts/005-job-procedure.sql
@@ -1,12 +1,13 @@
 CREATE PROCEDURE new_job(
     job_id job.job_id%TYPE,
     job_title job.job_title%TYPE,
-    min_salary job.min_salary%TYPE
+    min_salary job.min_salary%TYPE,
+    max_salary job.max_salary%TYPE DEFAULT NULL
 ) AS $$
 DECLARE
-    max_salary job.max_salary%TYPE := min_salary * 2;
+    v_max_salary max_salary%TYPE := COALESCE(max_salary, min_salary * 2);
 BEGIN
     INSERT INTO job (job_id, job_title, min_salary, max_salary)
-    VALUES (job_id, job_title, min_salary, max_salary);
+    VALUES (job_id, job_title, min_salary, v_max_salary);
 END;
 $$ LANGUAGE plpgsql;

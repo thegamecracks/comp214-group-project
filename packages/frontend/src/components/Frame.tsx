a6d6a6c63b34f2122b79f7fd83ae579f24faff46
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-12-02 06:24:02 -0500

    feat(frontend): hide navbar home when not logged in

--- packages/frontend/src/components/Frame.tsx
+++ packages/frontend/src/components/Frame.tsx
@@ -4,15 +4,16 @@ import { useAuth } from "@/lib/auth"
 
 export function Frame() {
   const auth = useAuth()
+  const loggedIn = auth.isAuthenticated()
 
   return (
     <div>
       <nav className="navbar flex">
-        <Link to="/" className="btn btn-ghost text-xl">Home</Link>
+        {loggedIn ? <Link to="/" className="btn btn-ghost text-xl">Home</Link> : <></>}
         <div className="flex-1" />
-        {!auth.isAuthenticated() ? <Link to="/login" className="btn btn-ghost text-xl">Login</Link> : <></>}
-        {!auth.isAuthenticated() ? <Link to="/login" className="btn btn-ghost text-xl">Register</Link> : <></>}
-        {auth.isAuthenticated() ? <button onClick={() => auth.clearAuth()} className="btn btn-ghost text-xl">Logout</button> : <></>}
+        {!loggedIn ? <Link to="/login" className="btn btn-ghost text-xl">Login</Link> : <></>}
+        {!loggedIn ? <Link to="/login" className="btn btn-ghost text-xl">Register</Link> : <></>}
+        {loggedIn ? <button onClick={() => auth.clearAuth()} className="btn btn-ghost text-xl">Logout</button> : <></>}
       </nav>
       <Outlet />
     </div>

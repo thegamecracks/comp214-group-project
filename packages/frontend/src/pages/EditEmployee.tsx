7f46659ff2dcaf99e6c28cbe12fc2be6c3c246a6
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-12-03 11:04:49 -0500

    refactor(frontend): extract useEmployee hook

--- packages/frontend/src/pages/EditEmployee.tsx
+++ packages/frontend/src/pages/EditEmployee.tsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import type { FormEvent } from "react"
 
-import { useAuth } from "@/lib/auth"
-import { useToast } from "@/lib/toast"
-import type { Department, Employee, Job } from "@/types"
+import { useEmployee } from "@/lib/state"
 
 export default function EditEmployee() {
   const { id } = useParams()
@@ -96,50 +94,4 @@ export default function EditEmployee() {
       </div>
     </form>
   )
-}
-
-function useEmployee(employee_id: string) {
-  const auth = useAuth()
-  const toast = useToast()
-  const [department, setDepartment] = useState<Department | null>(null)
-  const [employee, setEmployee] = useState<Employee | null>(null)
-  const [job, setJob] = useState<Job | null>(null)
-  const [manager, setManager] = useState<Employee | null>(null)
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const { data } = await auth.api.get(`/employees/${employee_id}`, { signal })
-        setEmployee(data)
-
-        const { department_id } = data
-        if (department_id) {
-          const { data } = await auth.api.get(`/departments/${department_id}`, { signal })
-          setDepartment(data)
-        }
-
-        const { job_id } = data
-        if (job_id) {
-          const { data } = await auth.api.get(`/jobs/${job_id}`, { signal })
-          setJob(data)
-        }
-
-        const { manager_id } = data
-        if (manager_id) {
-          const { data } = await auth.api.get(`/employees/${manager_id}`, { signal })
-          setManager(data)
-        }
-      } catch (error) {
-        toast.error(error)
-      }
-    }
-
-    const controller = new AbortController()
-    const signal = controller.signal
-    getData()
-
-    return () => controller.abort()
-  }, [])
-
-  return { department, employee, job, manager }
 }
